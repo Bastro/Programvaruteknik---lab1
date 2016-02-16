6b34a934bc4a3e8bd2a350fbaac8c6dbc64871c9
Jonas Öster <jonasoster@jonass-Mac-mini.local>
2016-02-16 17:49:02 +0100

    test

--- src/test/DataSourceImpTest.java
+++ src/test/DataSourceImpTest.java
@@ -37,13 +37,13 @@ public class DataSourceImpTest {
 	@Test
 	public void testData() {
 		ds.addData(LocalDate.of(0000, 01, 01), 0.0);
-		ds.addData(LocalDate.of(1999, 12, 31), 3.5);
+		ds.addData(LocalDate.of(1999, 12, 31), -3.5);
 		ds.addData(LocalDate.of(2000, 01, 01), 1.4);
 		ds.addData(LocalDate.of(2014, 11, 20), Double.MAX_VALUE);
 		ds.addData(LocalDate.of(2015, 10, 01), Double.MIN_VALUE);
 		Map<LocalDate, Double> map = ds.getData();
 		assertEquals(0.0, map.get(LocalDate.of(0000, 01, 01)), 0.001);
-		assertEquals(3.5, map.get(LocalDate.of(1999, 12, 31)), 0.001);
+		assertEquals(-3.5, map.get(LocalDate.of(1999, 12, 31)), 0.001);
 		assertEquals(1.4, map.get(LocalDate.of(2000, 01, 01)), 0.001);
 		assertEquals(Double.MAX_VALUE, map.get(LocalDate.of(2014, 11, 20)), 0.001);
 		assertEquals(Double.MIN_VALUE, map.get(LocalDate.of(2015, 10, 01)), 0.001);
